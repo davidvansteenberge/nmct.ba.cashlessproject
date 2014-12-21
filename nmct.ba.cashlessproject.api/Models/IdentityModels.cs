9df062148e9365a76250eff91d54517a8f1cae09
davidvansteenberge <david.van.steenberge@student.howest.be>
2014-12-21 13:02:30 +0100

    Aanpassingen in API
    settings voor db connectie
    views overgenomen van voorbeeldproject
    startup config

--- nmct.ba.cashlessproject.api/Models/IdentityModels.cs
+++ nmct.ba.cashlessproject.api/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -16,7 +17,15 @@ namespace nmct.ba.cashlessproject.api.Models
             // Add custom user claims here
             return userIdentity;
         }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
+       
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
